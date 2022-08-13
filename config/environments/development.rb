a8d457d3e515538d9da4ca6e1a774116f7a70e58
Trevor Vardeman <trevortx@gmail.com>
2022-08-13 10:19:59 -0700

    commit tree e348982f36edd9b4821815766a57ae9aa166296e

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,4 +55,12 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # config.hosts << "localhost"
+
+  # config.hosts << /[a-z0-9]+\.c9users\.io/
+
+  # Rails.application.configure do
+  #   config.hosts.clear
+  # end
 end
